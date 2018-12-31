97eea56caf7446947c23cd72a55afa6c50247cf7
online6731 <mohammad.parsian@gmail.com>
2019-01-01 02:06:59 +0330

    Gitacora saved 3 changed files

--- modules/regex speed test.py
+++ modules/regex speed test.py
@@ -1,4 +1,4 @@
 import re
 
-ans = re.findall(r"(sub.*?) ", "this subject has a submarine as a subsequence" * 10 ** 5)
+ans = re.findall(r"\\b(sub)([^ ]*)", "this subject has a submarine as a subsequence" * 10 ** 5)
 print(len(ans))
