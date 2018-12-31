86e9d9c529928c3d73d203bd49496639444f79a4
online6731 <mohammad.parsian@gmail.com>
2019-01-01 02:01:37 +0330

    Gitacora saved 3 changed files

--- modules/regex speed test.py
+++ modules/regex speed test.py
@@ -1,4 +1,4 @@
 import re
 
-ans = re.search(r"(sub[^ ]*)", "this subject has a submarine as a subsequence")
+ans = re.search(r"(sub[^ ]*) ", "this subject has a submarine as a subsequence")
 print(ans.groups())
