9eb3019aeed76ac8f997b0a61a03525c159890ef
online6731 <mohammad.parsian@gmail.com>
2019-01-01 02:00:04 +0330

    Gitacora saved 3 changed files

--- modules/regex speed test.py
+++ modules/regex speed test.py
@@ -1,4 +1,4 @@
 import re
 
-ans = re.search(r"\b(sub)([^ ]*)", "this subject has a submarine as a subsequence")
+ans = re.search(r"\b(sub)([^ ]*)", "this subject has a submarine as a subsequence" * 10)
 print(ans.groups())
