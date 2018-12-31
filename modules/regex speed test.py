97d668abc145a85bc49100843ee059fd48195ea8
online6731 <mohammad.parsian@gmail.com>
2019-01-01 02:04:50 +0330

    Gitacora saved 3 changed files

--- modules/regex speed test.py
+++ modules/regex speed test.py
@@ -1,4 +1,4 @@
 import re
 
-ans = re.findall(r"(sub.*?) ", "this subject has a submarine as a subsequence" * 1000)
+ans = re.findall(r"(sub.*?) ", "this subject has a submarine as a subsequence" * 10000)
 #print(ans)
