9c28266dcc40fa9c58a5e4877dd9aae2910386dc
online6731 <mohammad.parsian@gmail.com>
2019-01-01 16:39:25 +0330

    Gitacora saved 3 changed files

--- modules/DatabaseManager.py
+++ modules/DatabaseManager.py
@@ -449,14 +449,17 @@ resources	=   {
 					}
 				},
 				'goodreads': {
-					'book': 'https://www.goodreads.com/book/show/{data_id}'
+					'book': {
+						'book': 'https://www.goodreads.com/book/show/{data_id}'
+						,
+						'book_list': list(itertools.chain.from_iterable([[f'https://www.goodreads.com/book/popular_by_date/{i}/{j}' for j in range(1, 13)] for i in range(2010, 2018)]))
+					}
 					,
-					'book_list': list(itertools.chain.from_iterable([[f'https://www.goodreads.com/book/popular_by_date/{i}/{j}' for j in range(1, 13)] for i in range(2010, 2018)]))
-					,
-					'author': 'https://www.goodreads.com/author/show/{data_id}'
-					,
-					'author_list': list(itertools.chain.from_iterable([[f'https://www.goodreads.com/book/popular_by_date/{i}/{j}' for j in range(1, 13)] for i in range(2010, 2018)]))
-
+					'author': {
+						'author': 'https://www.goodreads.com/author/show/{data_id}'
+						,
+						'author_list': list(itertools.chain.from_iterable([[f'https://www.goodreads.com/book/popular_by_date/{i}/{j}' for j in range(1, 13)] for i in range(2010, 2018)]))
+					}
 				},
 				'cia': {
 					'country': 'https://www.cia.gov/library/publications/the-world-factbook/geos/{data_id}.html'
