4fdb3b2049ac34089ebf4aa70697464f7028c79e
online6731 <mohammad.parsian@gmail.com>
2019-01-02 22:55:10 +0330

    Gitacora saved 4 changed files

--- modules/DatabaseManager.py
+++ modules/DatabaseManager.py
@@ -417,7 +417,8 @@ def download_resources(resource, db_name, count_saves=float('Inf'), count_founds
 	start_time = time.time()
 	base = get_page_link(resource, db_name, 'base')
 	page_queue = get_page_link(resource, db_name, f'{db_name}_list') if page_queue is None else page_queue
-	for i, page in enumerate(page_queue[start:]):
+	i = start
+	while i < len(page_queue):
 		logger.info(f'Founded pages: {len(page_queue)} ------ Saved pages: {i}')
 		souped_page = make_soup(page)
 		patterns = [get_page_link(resource, db_name, f'{db_name}_pattern')]
