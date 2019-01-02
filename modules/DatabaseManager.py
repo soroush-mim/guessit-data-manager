9a3cdbfb2a281e6aa96d40ce763416462b08b6f7
online6731 <mohammad.parsian@gmail.com>
2019-01-03 00:38:57 +0330

    Gitacora saved 4 changed files

--- modules/DatabaseManager.py
+++ modules/DatabaseManager.py
@@ -416,7 +416,7 @@ def test_getter(data_name, resource, attr=None, test_count=20):
 def download_resources(resource, db_name, count_saves=float('Inf'), count_founds=float('Inf'), timeout=float('Inf'), page_queue=None, start=0):
 	start_time = time.time()
 	init_project()
-	location = f'{project_dir}/download/page/{resource}/{db_name}/'
+	location = f'{main_dir}/download/page/{resource}/{db_name}/'
 	base = get_page_link(resource, db_name, 'base')
 	page_queue = get_page_link(resource, db_name, f'{db_name}_list') if page_queue is None else page_queue
 	i = start
@@ -553,8 +553,8 @@ resources	=   {
 				}
 			}
 
-
-project_dir			= '/home/flc/guessit/guessit_data_manager/'
+main_dir			= '/home/flc/guessit'
+project_dir			= f'{main_dir}/guessit_data_manager'
 dataset_dir			= f'{project_dir}/data_manager/modules/datasets'
 process_count	   	= 4
 updating_step	   	= 10
