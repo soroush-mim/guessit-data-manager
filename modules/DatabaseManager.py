2ec2fc6e06f8b3a14e6ceb89b6427f16b265408b
online6731 <mohammad.parsian@gmail.com>
2019-01-02 06:20:59 +0330

    Gitacora saved 4 changed files

--- modules/DatabaseManager.py
+++ modules/DatabaseManager.py
@@ -436,7 +436,7 @@ resources	=   {
 				'imdb': {
 					'movie': {
 						'movie_list': [f'https://www.imdb.com/search/title?title_type=feature&count={250}&start={i + 1}' for i in range(0, 10000, 250)]
-									+ [f'https://www.imdb.com/search/title?title_type=feature&sort=num_votes,desc&count={250}&page={int((i+1)/250 + 1)}' for i in range(0, 10000, 250)]
+									+ [f'https://www.imdb.com/search/title?title_type=feature&sort=num_votes,desc&count={250}&start={i + 1}' for i in range(0, 10000, 250)]
 						,
 						'movie': 'https://www.imdb.com/title/{data_id}'
 						,
