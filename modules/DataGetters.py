36d633a36fa0766fb9945cf22e47f9087c499764
soroush-mim <soroushm1377@gmail.com>
2019-03-06 17:06:11 +0330

    all functions added to get_footballPlayer_data_from_sofifa class

--- modules/DataGetters.py
+++ modules/DataGetters.py
@@ -34,38 +34,194 @@ class get_footballPlayer_data_from_sofifa(Data_getter):
 		Data_getter.__init__(self , page)
 		self.main_table = page.find('div' , class_ = 'card card-border player fixed-width')
 		self.top_row = self.main_table.find('div' , class_ = 'meta')
+		self.columns = self.main_table.find_all('div' , class_ = 'columns' )[1].find_all('div' , class_ = 'column col-4')
+		self.left_column_elements = self.columns[0].find_all('li')
+		self.third_column = self.columns[2].find_all('li')
+		self.forth_column = []
+		self.hashtags_table = self.main_table.find('div' , class_ = 'mt-2').find_all('a')
+		self.like_table = self.main_table.find('div' , class_ = 'operation mt-2')   
+		if len(self.columns) > 3:
+			self.forth_column = self.columns[3].find_all('li')
 
 	@property
-	def shirt_name(self):
+	def getter_shirt_name(self):
 		return re.search(r'.*\(' , self.main_table.find('div' , class_ = 'info').text.strip()).group()[:-1]
 
 	@property
-	def name(self):
+	def getter_name(self):
 		return re.search(r'((.*?)  )' , self.top_row.text.strip()).group()[:-2].strip()
 
 	@property
-	def age(self):
+	def getter_age(self):
 		age = re.search(r'\d\d\d?' , self.top_row.text.strip()).group().strip()
 
 	@property
-	def nationality(self):
+	def getter_nationality(self):
 		return self.top_row.find('a' , {'href' : re.compile(r'\/players\?na.*')})['title']
 
 	@property
-	def photo_link(self):
+	def getter_photo_link(self):
 		return self.main_table.find('img' )['data-src']
 
 	@property
-	def id(self):
+	def getter_id(self):
 		return re.search(r'\d+' , self.main_table.find('div' , class_ = 'info').find('h1').text.strip()).group()
 
 	@property
-	def positions(self):
+	def getter_positions(self):
 		positions_str = re.search(r'  .*A' , self.top_row.text.strip()).group()[2:-1].strip()
 		return positions_str.split()
+	
+	@property
+	def getter_birth_date(self):
+		return re.search(r'\d \(.*, \d\d\d\d\)' ,self.top_row.text.strip()).group()[3:-1]
+
+	@property
+	def getter_weight_in_pond(self):
+		return re.search(r'\d?\d\d\dlbs',self.top_row.text.strip()).group().strip()
+	
+	@property
+	def getter_height_in_cnm(self):
+		dirty_height = re.search(r'\d\d?.\d\d?\"',self.top_row.text.strip()).group().strip()
+		return str(int(dirty_height[0]) * 30.28 + int(re.search(r'\'.*"' , dirty_height).group().strip()[1:-1]) * 2.54)
+	
+	@property
+	def getter_value(self):
+		return self.main_table.find('div' , class_ = 'card-body stats').find_all('div' , class_ = 'column col-4 text-center')[2].find('span').text.strip()
+
+	@property
+	def getter_wage(self):
+		 return self.main_table.find('div' , class_ = 'card-body stats').find_all('div' , class_ = 'column col-4 text-center')[3].find('span').text.strip()
+
+	@property
+	def getter_overall_rating(self):
+		return self.main_table.find('div' , class_ = 'card-body stats').find_all('div' , class_ = 'column col-4 text-center')[0].find('span').text.strip()
+
+	@property
+	def getter_potential(self):
+		return self.main_table.find('div' , class_ = 'card-body stats').find_all('div' , class_ = 'column col-4 text-center')[1].find('span').text.strip()
+
+	@property
+	def getter_foot(self):
+		return re.search(r'(Left)|(Right)' , self.left_column_elements[0].text).group().strip()
+
+	@property
+	def getter_International_Reputation(self):
+		return self.left_column_elements[1].text.strip()[-1]
+
+	@property
+	def getter_weak_foot_star(self):
+		return self.left_column_elements[2].text.strip()[-1]
+
+	@property
+	def getter_skill_moves(self):
+		return self.left_column_elements[3].text.strip()[-1]
+
+	@property
+	def getter_work_rate(self):
+		return self.left_column_elements[4].text.strip()[9:]
+
+	@property
+	def getter_body_type(self):
+		return self.left_column_elements[5].text.strip()[9:]
+
+	@property
+	def getter_real_face(self):
+		return self.left_column_elements[6].text.strip()[9:]
+	
+	@property
+	def getter_release_clause(self):
+		return self.left_column_elements[7].text.strip()[14:]
+	
+	@property
+	def getter_club_team(self):
+		return self.third_column[0].text.strip()
+
+	@property
+	def getter_club_team_id_sofifa(self):
+		return re.search(r'\/\d*\/' , self.third_column[0].find('a' , {'href' : re.compile(r'\/team\/.*')})['href']).group().strip()[1:-1]
+
+	@property
+	def getter_power_in_club(self):
+		return self.third_column[1].text.strip()
+
+	@property
+	def getter_Position_in_club(self):
+		return self.third_column[2].text.strip()[-2:]
+
+	@property
+	def getter_Jersey_Number_in_club(self):
+		return self.third_column[3].text.strip()[-2:]
+
+	@property
+	def getter_club_join_date(self):
+		return self.third_column[4].text.strip()[6:]
+
+	@property
+	def getter_Contract_Valid_Until(self):
+		return self.third_column[5].text.strip()[-4:]
+
+	@property
+	def getter_national_team(self):
+		if len(self.forth_column) > 0:
+			return self.forth_column[0].text.strip()
+		else:
+			return None
+
+	@property
+	def getter_national_team_id(self):
+		if len(self.forth_column) > 0:
+			return re.search(r'\/\d*\/' , self.forth_column[0].find('a' , {'href' : re.compile(r'\/team\/.*')})['href']).group().strip()[1:-1]
+		else:
+			return None
+
+	@property
+	def getter_power_in_national(self):
+		if len(self.forth_column) > 0:
+			return self.forth_column[1].text.strip()
+		else:
+			return None
+
+	@property
+	def getter_Position_in_national(self):
+		if len(self.forth_column) > 0:
+			return self.forth_column[2].text.strip()[-2:]
+		else:
+			return None
+
+	@property
+	def getter_Jersey_Number_in_national(self):
+		if len(self.forth_column) > 0:
+			return self.forth_column[3].text.strip()[-2:]
+		else:
+			return None
+
+	@property
+	def getter_abilities_hashtags(self):
+		return [i.text for i in self.hashtags_table]
+
+	@property
+	def getter_followers_num(self):
+		return self.like_table.find('a' , class_ = "follow-btn btn").find('span').text.strip()
+
+	@property
+	def getter_likes_num(self):
+		return self.like_table.find('a' , class_ = "like-btn btn").find('span').text.strip()
+
+	@property
+	def getter_dislikes_num(self):
+		return self.like_table.find('a' , class_ = "dislike-btn btn").find('span').text.strip()
+	
+	@property
+	def get_all_data(self):
+		data = {}
+		for i in dir(self):
+			if i.startswith('getter_'):
+				data[i[7:]] = getattr(self,i)
+		return data
 
 
-
+	# 38 funcs
 
 
 
