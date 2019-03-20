85ccea679611326a3507b3149b5ad43f5fbf7d4b
onlie6731 <mohammad.parsian@gmail.com>
2019-03-20 20:06:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ from template_engine.TemplateEngine import create_question
 
 app = Flask(__name__)
 
-URLS_CONST = {
-    'getQ':'get_question',
-    'chkAns':'check_answer',
-}
-
-
 
 @app.route('/', methods=['GET'])
 def main():
@@ -25,74 +19,10 @@ def main():
     Response = {
         'ok': 1,
         'message': 'question server is on',
-        'urls': list(URLS_CONST.values()),
+        'urls': [],
     }
 
     return json.dumps(Response)
-
-
-
-
-@app.route(f'/{URLS_CONST["getQ"]}', methods=['POST', 'GET'])
-def get_question():
-    '''
-    get question part of server
-
-    page context:
-        get the question py post request
-    '''
-    
-    Response = ''
-    
-    if request.method == 'POST':
-        Request = request.json
-
-
-        Response = Request
-    
-    elif request.method == 'GET':
-        Response = {
-            'ok': 0,
-            'error': 'please use post request for get question',
-            'request': {
-                'tag': 'some tag that you want',
-            }
-        }
-    
-    return json.dumps(Response)
-
-
-
-
-@app.route(f'/{URLS_CONST["chkAns"]}', methods=['POST', 'GET'])
-def check_answer():
-    '''
-    check the answer of a question
-
-    page context:
-        check the answer of question by a post request
-    '''
-    
-    Response = ''
-
-    if request.method == 'POST':
-        Request = request.json
-
-
-        Response = Request
-    
-    elif request.method == 'GET':
-        Response = {
-            'ok': 0,
-            'error': 'please use post request for check Ans',
-            'request': {
-                'id': 'question ID',
-                'answer': 'answer or answers',
-            }
-        }
-    
-    return json.dumps(Response)
-
 
 
 
