55a4ba7a885afe6a41145b23dac7d3c3bfc56dc8
danial keimasi <danialkeimasi@gmail.com>
2019-04-19 18:32:46 +0430

    Update argParse.py

--- modules/argParse.py
+++ modules/argParse.py
@@ -15,11 +15,30 @@ def arg_parse():
         type=str, nargs=2,
         help='run the download resource function\napp.py -dr <resource> <db_name>',
     )
+    parser.add_argument(
+        '-fd', '--finddb',
+        dest='finddb', default=None,
+        type=str, nargs=1,
+        help='Createss dataset with only id\napp.py -fd <db_name>',
+    )
+    parser.add_argument(
+        '-ud', '--updatedb',
+        dest='updatedb', default=None,
+        type=str, nargs=1,
+        help='Update all fields of dataset using data getterds\napp.py -ud <db_name>',
+    )
 
     args = parser.parse_args()
 
     if args.dlres:
         dbManager.download_resources(args.dlres[0], args.dlres[1])
+
+    if args.finddb:
+        dbManager.find_db(args.finddb[0])
+    
+    if args.updatedb:
+        dbManager.update_db(args.updatedb[0])
+    
 
     # if there is any arg, return True
     if (len(sys.argv) == 1) or (len(sys.argv) == 2 and sys.argv[1] =='-log'):
