6bc4ff3d74b3b729868468e6e0ed8f409b6d5789
danial keimasi <danialkeimasi@gmail.com>
2019-04-19 18:39:45 +0430

    Update argParse.py

--- modules/argParse.py
+++ modules/argParse.py
@@ -1,5 +1,5 @@
 import argparse
-
+import sys
 import modules.DatabaseManager as dbManager
 
 def arg_parse():
@@ -13,19 +13,25 @@ def arg_parse():
         '-dr', '--dlres',
         dest='dlres', default=None,
         type=str, nargs=2,
-        help='run the download resource function\napp.py -dr <resource> <db_name>',
+        help='run the download resource function\n app.py -dr <resource> <db_name>',
     )
     parser.add_argument(
         '-fd', '--finddb',
         dest='finddb', default=None,
         type=str, nargs=1,
-        help='Createss dataset with only id\napp.py -fd <db_name>',
+        help='Createss dataset with only id\n app.py -fd <db_name>',
     )
     parser.add_argument(
         '-ud', '--updatedb',
         dest='updatedb', default=None,
         type=str, nargs=1,
-        help='Update all fields of dataset using data getterds\napp.py -ud <db_name>',
+        help='Update all fields of dataset using data getterds\n app.py -ud <db_name>',
+    )
+    parser.add_argument(
+        '-inpr', '--initproject',
+        dest='initproject', default=None,
+        type=str, nargs='*',
+        help='initialize project and prepare folder structure\n app.py -inpr <db_name>',
     )
 
     args = parser.parse_args()
@@ -39,6 +45,8 @@ def arg_parse():
     if args.updatedb:
         dbManager.update_db(args.updatedb[0])
     
+    if args.initproject:
+        dbManager.init_project()
 
     # if there is any arg, return True
     if (len(sys.argv) == 1) or (len(sys.argv) == 2 and sys.argv[1] =='-log'):
