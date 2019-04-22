992b5a7ffec667c516da33b866e1df97b33de659
danial keimasi <danialkeimasi@gmail.com>
2019-04-22 19:09:50 +0430

    move old arg parse from DatabaseManager.py file to argParse.py file

--- modules/argParse.py
+++ modules/argParse.py
@@ -53,3 +53,75 @@ def arg_parse():
         return False
     else:
         return True
+
+
+def old_arg_parse():
+    """
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--test_getter', action='store_true', dest='test_getter',
+                        default=False, help='test getter modules')
+
+    parser.add_argument('--download_resources', action='store_true', dest='download_resources',
+                        default=False, help='download resources')
+
+    parser.add_argument('--update_db', action='store_true', dest='update_db',
+                        default=False, help='updating database')
+
+    parser.add_argument('--find_db', action='store_true', dest='find_db',
+                        default=False, help='finding database id')
+
+    parser.add_argument('--init_db', action='store_true', dest='init_db',
+                        default=False, help='initializing database')
+
+    parser.add_argument('-resource', type=str, dest='resource',
+                        help='resource of test data')
+
+    parser.add_argument('-db', type=str, dest='db',
+                        help='db of test data')
+
+    parser.add_argument('-attributes', type=str, nargs='+', dest='attributes', default=None,
+                        help='attribute of test data')
+
+    parser.add_argument('-count', type=int, dest='count', default=None,
+                        help='number of test datas')
+
+    parser.add_argument('-id', type=str, nargs='+', dest='id',
+                        help='data id s to be tested')
+
+    parser.add_argument('-resume', action='store_true', dest='resume', default=False,
+                        help='specify the resume arg in download_resources function')
+
+    parser.add_argument('-dont_use_local_save', action='store_true', dest='dont_use_local_save', default=False,
+                        help='specify whether should use local saved pages in make_spup funtion or not')
+
+    parser.add_argument('-dont_save_page_local', action='store_true', dest='dont_save_page_local', default=False,
+                        help='specify whether should use local saved pages in make_spup funtion or not')
+
+    parser.add_argument('-complete_report', action='store_true', dest='complete_report', default=False,
+                        help='gives complete eport of task')
+
+    args = parser.parse_args()
+
+    config.use_local_save = not args.dont_use_local_save
+    config.save_page_local = not args.dont_save_page_local
+
+    if args.test_getter:
+        logger.critical('test results : ')
+        pprint(test_getter(args.db, args.resource, args.attributes, count=args.count, id_list=args.id,
+                           complete_report=args.complete_report))
+
+    elif args.download_resources:
+        download_resources(args.resource, args.db, count=args.count, resume=args.resume)
+
+    elif args.update_db:
+        update_db(args.db)
+
+    elif args.find_db:
+        find_db(args.db)
+
+    elif args.init_db:
+        init_db(args.db)
+
