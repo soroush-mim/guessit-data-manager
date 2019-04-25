defd9538817395a89467a88090c079006198fd68
danial keimasi <danialkeimasi@gmail.com>
2019-04-26 01:25:17 +0430

    Update __tools.py

--- modules/data_getters/__tools.py
+++ modules/data_getters/__tools.py
@@ -1,10 +1,30 @@
 
 
-money_value = lambda money : int(float(money[1:-1]) * (10 ** (3 if money[-1] == 'K' else 6 if money[-1] == 'M' else 0))) if money != '€0' else 0
+def money_value(money):
+    """ change the number of money to a standard form """
 
-date_value =  lambda date  : (int(date[-4:]), int([value for key, value in \
-                                    {'January'	: 1 , 'February': 2	, 'March'		: 3,
-                                    'April'		: 4 , 'May'		: 5	, 'June'		: 6,
-                                    'July'		: 7 , 'August'	: 8	, 'September'	: 9,
-                                    'October'	: 10, 'November': 11, 'December'	: 12
-                                    }.items() if date[:3] in key][0]), int(date[date.find(' ') + 1:date.find(',')]))
+    return int(
+        float(money[1:-1]) * (10 ** (3 if money[-1] == 'K' else
+                                     6 if money[-1] == 'M' else 0))) if money != '€0' else 0
+
+
+
+def date_value(date):
+    """
+    get the standard type of date
+
+    :param date:
+    :return: tumple
+    """
+
+    months = \
+        {'January'      : 1,  'February': 2, 'March'	    : 3,
+         'April'		: 4 , 'May'		: 5	, 'June'		: 6,
+         'July'		    : 7 , 'August'	: 8	, 'September'	: 9,
+         'October'	    : 10, 'November': 11, 'December'	: 12}
+
+    return (int(date[-4:]),
+            int([value for key, value in months.items() if date[:3] in key][0]),
+            int(date[date.find(' ') + 1: date.find(',')]))
+
+
