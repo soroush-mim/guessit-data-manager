9e43a9106bf2a0c3dcc8b3f99fef31affb4b85ff
danial keimasi <danialkeimasi@gmail.com>
2019-04-27 19:31:43 +0430

    Update __baseClass.py

--- modules/data_getters/__baseClass.py
+++ modules/data_getters/__baseClass.py
@@ -2,13 +2,13 @@ from modules.config.config import logger
 
 
 class DataGetterBaseClass:
-    """a parent class for all data getters classes that get page soup file for input"""
+    """ a parent class for all data getters classes that get page soup file for input """
 
     def __init__(self, page):
         self.page = page
 
     def get_all_data(self):
-        """a function for getting all data of a player and put it in a dictionary"""
+        """ a function for getting all data of a player and put it in a dictionary """
         data = {}
         for _property in [x for x in dir(self) if x.startswith('getter_')]:
 
